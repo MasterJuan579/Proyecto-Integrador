143bd3a39cffb62860ab2cfad4e82a056c626ed6
MasterJuan579 <juanperezgutierrez579@gmail.com>
2023-10-24 01:23:11 -0600

    Acabado Final

--- Proyecto.py
+++ Proyecto.py
@@ -14,10 +14,10 @@ def Pedir_Datos_Inventario() -> list:
     Precio = float(input("Ingresé su precio: "))
     while True:
         cantidad_input = input("Ingrese la Cantidad Disponibles (número entero positivo): ")
-        if cantidad_input.isdigit():  # Comprueba si la entrada es un número entero positivo
+        if cantidad_input.isdigit(): 
             Cantidad = int(cantidad_input)
             if Cantidad >= 1:
-                break  # Si es válido, salir del bucle
+                break 
             else:
                 print("La cantidad debe ser un número entero positivo.")
         else:
@@ -41,8 +41,7 @@ def Pedir_Datos_Vendedor()-> list:
     return Datos    
 
 
-def Registrar(Nombre_archivo: str, Datos:list )-> None:
-    # Registra Nombre Producto, Vendedor, Precio, Cantidad Existencia, Ventas, Llegada al Almacen 
+def Registrar(Nombre_archivo: str, Datos:list )-> None: 
     with open(Nombre_archivo, "a", newline="\n", encoding="utf8") as archivo:
         escritor = csv.writer(archivo)
         escritor.writerow(Datos)
@@ -62,24 +61,26 @@ def Consulta_Avanzada(Nombre_Archivo: str, Nombre: str) -> list:
                 Nombre_en_fila = fila[0].strip().lower()
                 if Nombre_en_fila == Busqueda:
                     Resultado.append(fila)
-                    break  # Rompe el bucle después de encontrar el primer resultado
+                    break  
 
         if not Resultado:
             print("El producto no se encuentra en la base de datos")
         else:
-            # Combina el encabezado y el producto encontrado en una sola lista
+            
             datos_a_imprimir = [Lista[0]] + Resultado
 
-            # Imprimir el encabezado y el producto encontrado en formato de celdas
-            Imprimir_Lista(datos_a_imprimir)
+    
+    
 
     return Resultado
     
 
 def Eliminar(Nombre_Archivo: str, Nombre: str)->None:
     # Elimina un producto junto con todas las caracteristicas
-    
+    Lista = leer_csv(Nombre_Archivo)
     Resultado = Consulta_Avanzada(Nombre_Archivo, Nombre)
+    datos_a_Imprimir = datos_a_imprimir = [Lista[0]] + Resultado
+    Imprimir_Lista(datos_a_imprimir)
 
     if not Resultado:
         return
@@ -166,11 +167,11 @@ def Imprimir_Lista(Datos: list) -> None:
 
              
 def Modificar_Producto_Inventario(Nombre_Archivo: str, Nombre: str, cantidad_vendida: int) -> None:
-    # Obtiene la lista completa de productos desde el archivo "Inventario.csv"
     lista = leer_csv(Nombre_Archivo)
     
-    # Busca el producto a modificar
     Consulta = Consulta_Avanzada(Nombre_Archivo, Nombre)
+    datos_a_Imprimir = datos_a_imprimir = [lista[0]] + Consulta
+    Imprimir_Lista(datos_a_imprimir)
     
     if not Consulta:
         print("No se encuentra el producto a modificar")
@@ -196,25 +197,32 @@ def Modificar_Producto_Inventario(Nombre_Archivo: str, Nombre: str, cantidad_ven
     # Actualiza la lista con la nueva información
     lista[index] = Consulta[0]
 
-    # Escribe la lista actualizada en el archivo "Inventario.csv"
     with open(Nombre_Archivo, "w", encoding="utf8", newline="") as archivo:
         escritor = csv.writer(archivo)
         escritor.writerows(lista)
 
     print("Se ha modificado la cantidad del producto correctamente.")
     Imprimir_Lista(lista)
-            
-    
+              
 
 def Actualizar_Ventas(Nombre_Archivo: str) -> None:
+    Lista_Inventario = leer_csv("Inventario.csv")
+    Lista_Vendedor = leer_csv("Vendedores.csv")
+    
     Vendedor = input("Ingrese el Nombre del Vendedor: ")
     Consulta_Vendedor = Consulta_Avanzada("Vendedores.csv", Vendedor)
+    datos_a_Imprimir = datos_a_imprimir = [Lista_Vendedor[0]] + Consulta_Vendedor
+    Imprimir_Lista(datos_a_imprimir)
+    
     if not Consulta_Vendedor:
         print(f"El vendedor '{Vendedor}' no se encuentra en la base de datos de vendedores.")
         return
 
     Producto = input("Ingrese el Nombre del Producto que vendió: ")
     Consulta_Producto = Consulta_Avanzada("Inventario.csv", Producto)
+    datos_a_Imprimir = datos_a_imprimir = [Lista_Inventario[0]] + Consulta_Producto
+    Imprimir_Lista(datos_a_imprimir)
+    
     if not Consulta_Producto:
         print(f"El producto '{Producto}' no se encuentra en el inventario.")
         return
@@ -225,10 +233,10 @@ def Actualizar_Ventas(Nombre_Archivo: str) -> None:
     Fecha = f"{Dia}-{Mes}-{Año}"
     while True:
         cantidad_input = input("Ingrese la Cantidad que Vendió (número entero positivo): ")
-        if cantidad_input.isdigit():  # Comprueba si la entrada es un número entero positivo
+        if cantidad_input.isdigit():  
             Cantidad = int(cantidad_input)
             if Cantidad >= 1:
-                break  # Si es válido, salir del bucle
+                break  
             else:
                 print("La cantidad debe ser un número entero positivo.")
         else:
@@ -241,9 +249,166 @@ def Actualizar_Ventas(Nombre_Archivo: str) -> None:
     
     Modificar_Producto_Inventario("Inventario.csv", Producto, Cantidad)
     
+
+def Ventas_por_Vendedor(Nombre_Archivo_Ventas: str, Nombre_Archivo_Inventario: str) -> None:
+    datos_ventas = leer_csv(Nombre_Archivo_Ventas)
     
+    # Crear un diccionario para almacenar las ventas y comisiones por vendedor
+    ventas_por_vendedor = {}
+    
+    for fila in datos_ventas[1:]:  # Excluir el encabezado
+        nombre_vendedor = fila[1]
+        cantidad_vendida = int(fila[2])
+        
+        # Consultar el precio del producto en el inventario
+        producto = fila[0]
+        consulta_producto = Consulta_Avanzada(Nombre_Archivo_Inventario, producto)
+        
+        if not consulta_producto:
+            print(f"No se encontró el producto '{producto}' en el inventario.")
+            continue
+        
+        precio_producto = float(consulta_producto[0][1])
+        
+        # Calcular el total de ventas y la comisión
+        total_ventas = cantidad_vendida * precio_producto
+        comision = total_ventas * 0.20
+        
+        # Actualizar el diccionario de ventas por vendedor
+        if nombre_vendedor in ventas_por_vendedor:
+            ventas_por_vendedor[nombre_vendedor] += total_ventas
+        else:
+            ventas_por_vendedor[nombre_vendedor] = total_ventas
+        
+    resultados = [["Nombre Vendedor", "Total Ventas", "Total Comisiones"]]
+    for nombre, ventas in ventas_por_vendedor.items():
+        comision = ventas * 0.20
+        resultados.append([nombre, ventas, comision])
+    
+    with open("Ventas_por_Vendedor.csv", "w", encoding="utf8", newline="") as archivo:
+        escritor = csv.writer(archivo)
+        escritor.writerows(resultados)
+    
+    Imprimir_Lista(resultados)
+
+
+def Ventas_por_Producto(Nombre_Archivo_Ventas: str, Nombre_Archivo_Inventario: str):
+    datos_ventas = leer_csv(Nombre_Archivo_Ventas)
+    inventario = leer_csv(Nombre_Archivo_Inventario)
+    
+    # Crear un diccionario para almacenar las ventas por producto
+    ventas_por_producto = {}
+    
+    for fila in datos_ventas[1:]:  # Excluir el encabezado
+        nombre_producto = fila[0]
+        cantidad_vendida = int(fila[2])
+        
+        # Consultar el precio del producto en el inventario
+        consulta_producto = Consulta_Avanzada(Nombre_Archivo_Inventario, nombre_producto)
+        
+        if not consulta_producto:
+            print(f"No se encontró el producto '{nombre_producto}' en el inventario.")
+            continue
+        
+        precio_producto = float(consulta_producto[0][1])
+        
+        # Calcular el total de ventas
+        total_ventas = cantidad_vendida * precio_producto
+        
+        # Actualizar el diccionario de ventas por producto
+        if nombre_producto in ventas_por_producto:
+            ventas_por_producto[nombre_producto][0] += cantidad_vendida
+            ventas_por_producto[nombre_producto][1] += total_ventas
+        else:
+            ventas_por_producto[nombre_producto] = [cantidad_vendida, total_ventas]
+    
+    resultados = [["Nombre Producto", "Cantidad Vendida", "Venta Total"]]
+    for producto, datos in ventas_por_producto.items():
+        cantidad_vendida, venta_total = datos
+        resultados.append([producto, cantidad_vendida, venta_total])
+    
+    with open("Ventas_por_Producto.csv", "w", encoding="utf8", newline="") as archivo:
+        escritor = csv.writer(archivo)
+        escritor.writerows(resultados)
+        Imprimir_Lista(resultados)
     
 
+def Calcular_Ventas_Totales(Nombre_Archivo_Ventas: str, Nombre_Archivo_Inventario: str):
+    datos_ventas = leer_csv(Nombre_Archivo_Ventas)
+
+    # Crear un diccionario para almacenar las ventas por vendedor
+    ventas_por_vendedor = {}
+
+    for fila in datos_ventas[1:]:  # Excluir el encabezado
+        nombre_vendedor = fila[1]
+        cantidad_vendida = int(fila[2])
+
+        # Consultar el precio del producto en el inventario
+        producto = fila[0]
+        consulta_producto = Consulta_Avanzada(Nombre_Archivo_Inventario, producto)
+
+        if not consulta_producto:
+            print(f"No se encontró el producto '{producto}' en el inventario.")
+            continue
+
+        precio_producto = float(consulta_producto[0][1])
+
+        # Calcular el total de ventas y la comisión
+        total_ventas = cantidad_vendida * precio_producto
+        comision = total_ventas * 0.20
+
+        # Actualizar el diccionario de ventas por vendedor
+        if nombre_vendedor in ventas_por_vendedor:
+            ventas_por_vendedor[nombre_vendedor][0] += total_ventas
+            ventas_por_vendedor[nombre_vendedor][1] += comision
+        else:
+            ventas_por_vendedor[nombre_vendedor] = [total_ventas, comision]
+
+    # Calcular el total de productos vendidos y el total de comisiones
+    total_productos_vendidos = sum(int(fila[2]) for fila in datos_ventas[1:])
+    total_comisiones = sum(comision for _, comision in ventas_por_vendedor.values())
+
+    # Calcular el total de ventas totales
+    total_ventas_totales = sum(total for total, _ in ventas_por_vendedor.values())
+
+    # Calcular el resultado final (Ventas Totales menos Comisiones)
+    resultado_final = total_ventas_totales - total_comisiones
+
+    # Crear una lista con los resultados
+    resultados = [
+        ["Total Productos Vendidos", total_productos_vendidos],
+        ["Total Ventas Totales", total_ventas_totales],
+        ["Total Comisiones", total_comisiones],
+        ["Resultado Final (Ventas Totales - Comisiones)", resultado_final]
+    ]
+
+    with open("Ventas_Totales.csv", "w", encoding="utf8", newline="") as archivo:
+        escritor = csv.writer(archivo)
+        escritor.writerows(resultados)
+
+    Imprimir_Lista(resultados)
+      
+
+
+def menu_Reportes():
+    while (opcion := input("""
+    1. Reporte Ventas por Vendedor 
+    2. Reporte Ventas por Producto
+    3. Reporte Ventas Totales
+    4. Regresar a Menu Principal
+    
+    Ingrese su Opcion: """)) != "4":
+        if opcion == "1":
+            Ventas_por_Vendedor("Ventas_Vendedores.csv", "Inventario.csv")
+        elif opcion == "2":
+            Ventas_por_Producto("Ventas_Vendedores.csv", "Inventario.csv")
+        elif opcion == "3":
+            Calcular_Ventas_Totales("Ventas_Vendedores.csv", "Inventario.csv",)
+        elif opcion == "4":
+            menu_principal()
+        else:
+            print("Ingrese una de las opciones") 
+        
 
 def menu_vendedores():
     while (opcion := input("""
@@ -274,7 +439,43 @@ def menu_vendedores():
         else: 
             print("Eliga una de las opciones: ")
         
+
+def menu_inventario():
+    while(opcion := input(""" MENU
+    1. Registro Producto
+    2. Consulta Avanzada 
+    3. Eliminar Producto
+    4. Ordenar Inventario
+    5. Regresar Menu Principal
+    
+    Introduzca su opción: """)) != "5": 
         
+        if opcion == "1":
+            Datos = Pedir_Datos_Inventario()
+            Registrar("Inventario.csv", Datos)
+            
+        elif opcion == "2":
+            Nombre = input("Ingrese el nombre del producto: ")
+            Lista = leer_csv("Inventario.csv")
+            Resultado = Consulta_Avanzada("Inventario.csv", Nombre)
+            datos_a_Imprimir = datos_a_imprimir = [Lista[0]] + Resultado
+            Imprimir_Lista(datos_a_imprimir)
+            
+        elif opcion == "3":
+            Nombre = input("Ingrese el Nombre del Producto que desee Eliminar: ")
+            Eliminar("Inventario.csv", Nombre)
+            
+        
+        elif opcion == "4":
+            Ordenar_Inventario("Inventario.csv")
+        
+        elif opcion == "5":
+            menu_principal()
+        
+        else: 
+            print("Elija una de las opciones del Menu")
+
+
 def menu_principal():
     while (opcion := input("""
     1. Inventario
@@ -290,51 +491,15 @@ def menu_principal():
             menu_vendedores()
             
         elif opcion == "3":
-            pass 
+            menu_Reportes()
+    
         elif opcion == "4":
             print("Hasta Luego")
+        
         else: 
             print("Introduzca una de las opciones ")
-        
-
-def menu_inventario():
-    while(opcion := input(""" MENU
-    1. Registro Producto
-    2. Consulta Avanzada 
-    3. Eliminar Producto
-    4. Modificar Producto
-    5. Ordenar Inventario
-    6. Regresar Menu Principal
+  
     
-    Introduzca su opción: """)) != "6": 
-        
-        if opcion == "1":
-            Datos = Pedir_Datos_Inventario()
-            Registrar("Inventario.csv", Datos)
-            
-        elif opcion == "2":
-            Nombre = input("Ingrese el nombre del producto: ")
-            Consulta_Avanzada("Inventario.csv", Nombre)
-            
-        elif opcion == "3":
-            Nombre = input("Ingrese el Nombre del Producto que desee Eliminar: ")
-            Eliminar("Inventario.csv", Nombre)
-            
-        elif opcion == "4":
-            Nombre = input("Ingrese el nombre del producto: ")
-            Modificar_Producto("Inventario.csv", Nombre)
-            pass
-        
-        elif opcion == "5":
-            Ordenar_Inventario("Inventario.csv")
-        
-        elif opcion == "6":
-            menu_principal()
-        
-        else: 
-            print("Elija una de las opciones del Menu")
-    
-
 if __name__ == "__main__":
     menu_principal()
 
